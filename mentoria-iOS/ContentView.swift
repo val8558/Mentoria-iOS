10b9aee5d8d8db250ac474413243bb727865cb5d
Valmir Garcia <val8558@gmail.com>
2024-07-15 20:52:49 -0300

    Initial Commit

---
+++ mentoria-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mentoria-iOS
+//
+//  Created by Valmir Garcia on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mentoria-iOS/mentoria_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  mentoria_iOSApp.swift
+//  mentoria-iOS
+//
+//  Created by Valmir Garcia on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct mentoria_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
