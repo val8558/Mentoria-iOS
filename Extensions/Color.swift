2c1144bbdfa74052a06283b5cfe89dbb6587f398
Valmir Garcia <111082887+val8558@users.noreply.github.com>
2024-08-12 17:08:24 -0300

    Update Color.swift

--- Extensions/Color.swift
+++ Extensions/Color.swift
@@ -10,7 +10,6 @@ import UIKit
 
 extension UIColor {
     
-   static let backGroundColor = UIColor(_colorLiteralRed: 21/255, green: 21/255, blue: 21/255, alpha: 1.0)
-    
-    static let titleColor = UIColor(_colorLiteralRed: 169/255, green: 147/255, blue: 33/255, alpha: 1.0)
+static let backGroundColor = UIColor(_colorLiteralRed: 21/255, green: 21/255, blue: 21/255, alpha: 1.0)
+static let titleColor = UIColor(_colorLiteralRed: 169/255, green: 147/255, blue: 33/255, alpha: 1.0)
 }
